0fd699ba201092ac90a5a830601f7aef2b2e8324
TalaDeepak <panghaldee@gmail.com>
2023-03-12 16:26:04 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '57a0925e-bd2e-442a-8d82-49fd50e91c11';
+export const MODAL_CLOSE_SEC = 2.5;
